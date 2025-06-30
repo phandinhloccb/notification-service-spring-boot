f598eb49ebb698c6323b5c454b934d2dec3aa8bf
l-phan <l-phan@creationline.com>
2025-06-30 16:41:30 +0900

    base kotlin vs kafka schema

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("com.github.davidmc24.gradle.plugin.avro") version "1.8.0"
+}
+
+group = "com.loc"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.kafka:spring-kafka")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// avro generate
+	implementation("org.apache.avro:avro:1.11.4")
+}
+
+
+val generatedResourcesDir = "${layout.buildDirectory.get()}/generated-resources"
+
+// Avro configuration
+tasks.named<com.github.davidmc24.gradle.plugin.avro.GenerateAvroJavaTask>("generateAvroJava") {
+	source("src/main/resources/static/avro")
+	setOutputDir(file("$generatedResourcesDir/avro"))
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
